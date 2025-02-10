43fb729c8346ba001833f6c52e78d67d4435925c
revanramdani31 <permanaevan09@gmail.com>
2025-02-10 23:03:19 +0700

    sudah ganti

---
+++ graphing.cpp
@@ -0,0 +1,18 @@
+#include <graphics.h>
+#include <conio.h>
+
+int main() {
+    // Inisialisasi mode grafik
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+
+    // Gambar lingkaran di tengah layar
+    circle(320, 240, 50);
+
+    // Tahan layar agar tidak langsung tertutup
+    getch();
+    
+    // Tutup mode grafik
+    closegraph();
+    return 0;
+}
